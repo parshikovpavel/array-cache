250bbd0849f9517e450552cd722e9fb468369ae2
pparshikov <paw-p@yandex.ru>
2019-09-16 19:49:30 +0300

    Modified Psr6->Psr16 adapter by some methods

--- src/Psr6ToPsr16Adapter.php
+++ src/Psr6ToPsr16Adapter.php
@@ -59,6 +59,76 @@ final class Psr6ToPsr16Adapter implements CacheInterface
      */
     public function set($key, $value, $ttl = null): bool
     {
+        $item = $this->pool->getItem($key);
+
+        $item->set($value)
+             ->expiresAfter($ttl);
+
+        return $this->pool->save($item);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete($key): bool
+    {
+        return $this->pool->deleteItem($key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function clear(): bool
+    {
+        return $this->pool->clear();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        $result = [];
+
+        //TODO Not completed
+
+
+        if (!is_array($keys)) {
+            if (!$keys instanceof \Traversable) {
+                throw new InvalidArgumentException('$keys is neither an array nor a Traversable');
+            }
+
+            $keys = iterator_to_array($keys, false);
+        }
+
+
+        $items = $this->pool->getItems($keys);
+
+        foreach ($items as $key => $item) {
+            $result[$key] = $item->isHit() ? $item->get() : $default;
+        }
+        return $result;
+    }
+
+    /**
+     * Validate iterable parameters of xxxMultiple functions
+     *
+     * An iterable parameter must be either an array or a Traversable
+     * otherwise `InvalidArgumentException` is thrown
+     *
+     * @param $iterable The iterable parameter
+     * @throws InvalidArgumentException Thrown if the iterable parameter is invalid
+     * @return void
+     */
+    private function validateIterable($iterable): void
+    {
+        if (!is_array($iterable) && !($iterable instanceof \Traversable)) {
+            throw new InvalidArgumentException('Invalid iterable parameter');
+        }
+    }
+
+
+
 
 
 }
